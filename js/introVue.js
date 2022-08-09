257e069f8525252253c6058554effd7d3298d215
Lucas Salvatori <lucas.salvatori@somoscoders.org>
2022-08-09 12:29:37 -0300

    Introduccion a Vue

---
+++ js/introVue.js
@@ -0,0 +1,66 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hola Vue!'
+  }
+})
+
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'Usted cargó esta página el ' + new Date().toLocaleString()
+  }
+})
+
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen: true
+  }
+})
+
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: 'Aprender JavaScript (callbacks)' },
+      { text: 'Aprender Vue' },
+      { text: 'Construir algo increíble' }
+    ]
+  }
+})
+
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hola Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
+
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: 'Hola Vue!'
+  }
+})
+
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetales' },
+      { id: 1, text: 'Queso' },
+      { id: 2, text: 'Cualquier otra cosa que se supone que los humanos coman' }
+    ]
+  }
+})
